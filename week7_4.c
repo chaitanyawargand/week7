8f3be484cd1ece0fa5b75f03cedee770928fefd9
chaitanyawargand <chaitanyawargand@gmail.com>
2024-11-27 13:53:15 +0530

    Update week7_4.c

--- week7_4.c
+++ week7_4.c
@@ -2,18 +2,19 @@
 
 int main()
 {  int k=0,n,n1;
-    int *a=(int*)calloc(30,sizeof(int));
     printf("enter the number of elements\n");
     scanf("%d",&n);
+   int *a=(int*)calloc(n,sizeof(int));
    printf("enter the array 1");
     for(int i=0;i<n;i++)
     scanf("%d",&a[i]);
-    int *b=(int*)calloc(10,sizeof(int));
      printf("enter the number of elements\n");
     scanf("%d",&n1);
+ int *b=(int*)calloc(n1,sizeof(int));
     printf("enter the array 2");
     for(int i=0;i<n1;i++)
     scanf("%d",&b[i]);
+ a=(int*)realloc(a,(n+n1));
     for(int i=0;i<n1;i++)
     a[n+i]=b[i];
    for(int i=0;i<n+n1;i++)
